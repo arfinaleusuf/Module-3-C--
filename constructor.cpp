b9ff7c8f52633292b82fd31b14afa7c3aca6de51
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-19 00:29:30 +0600

    constractor using

---
+++ constructor.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+    public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int r, int c, double g) // constructor
+
+    /* eta speacial type er function
+    1. kichi return kore na;
+    2. ei function er nam class er nam er hubu hu rakhte hoy
+
+    */
+    {
+        roll = r;
+        cls = c;
+        gpa = g;
+    }
+};
+int main()
+{
+    Student rohim(45,5,3.25);
+
+    Student korim(2,5,5);
+
+
+    cout<< rohim.roll<< " " << rohim.cls << " " << rohim.gpa << endl;
+    cout<< korim.roll<< " " << korim.cls << " " << korim.gpa << endl;
+    return 0;
+}

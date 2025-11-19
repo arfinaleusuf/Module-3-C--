c71aef6cf36b2d2cabb6581b5c727921e5a85aee
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-19 19:43:31 +0600

    shifting zero

---
+++ N_Shift_Zeros.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n = 0;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for(int j = 0; j<n-1 ;j++)
+    {
+        for (int i = 0; i < n-1; i++)
+        {
+            if (a[i] == 0)
+            {
+                swap(a[i], a[i + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}

0fcb27ddcd5a7b9e7c08e3e2b9278444b2df45e1
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-19 10:40:35 +0600

    dynamic array return from function

---
+++ dynamic_object_return_from_function.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa)
+    {
+        this->roll = roll;
+        this->cls = cls;
+        this->gpa = gpa;
+    }
+};
+
+Student* fun()
+{
+    Student* korim = new Student (2, 5, 5.00);
+    return korim;
+}
+int main()
+{
+    Student *p = fun();
+
+    cout << p->roll << " " << p->cls << " " << p->gpa << endl;
+    return 0;
+}
